df24a40055c83e109ba7fb189de4a794362069c4
erodrigufer <erodrigufer@gmail.com>
2023-04-19 18:01:57 +0200

    Highlight the code snippets of M2HTML component

--- src/components/Md2HTML.tsx
+++ src/components/Md2HTML.tsx
@@ -1,14 +1,36 @@
-import React from "react";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface Props {
   markdownText: string;
 }
 
 const Md2HTML = (props: Props) => {
+  const styling = oneLight;
   return (
     <div>
-      <ReactMarkdown>{props.markdownText}</ReactMarkdown>
+      <ReactMarkdown
+        children={props.markdownText}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, "")}
+                style={styling}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </div>
   );
 };
