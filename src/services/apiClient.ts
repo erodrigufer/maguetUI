f806c2a78c07c64aca03af0fbc4285b87e1086b5
erodrigufer <erodrigufer@gmail.com>
2023-04-25 13:54:50 +0200

    Uses timeouts in axios

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,6 +2,8 @@ import axios, { CanceledError } from "axios";
 
 export default axios.create({
     baseURL: 'http://localhost:8000',
+    timeout: 120_000, // Timeout of 2min (in ms).
+    timeoutErrorMessage: "error: a timeout happened",
     withCredentials: false,
     // headers: {
     //     'api-key': 'key',
